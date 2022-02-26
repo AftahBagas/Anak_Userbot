ea206c89a97aaaa6b655c723e313b31e4a0f1e61
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2022-02-26 13:33:52 +0700

    Update repo.py

--- Panda/plugins/repo.py
+++ Panda/plugins/repo.py
@@ -15,5 +15,5 @@ async def _(event):
     "animation command"
     event = await edit_or_reply(
         event,
-        "**╭━━━━━━━━━━━╮**\n\n [Repository](https://github.com/AftahBagas/new_userbot)\n**╰━━━━━━━━━━━╯**\n **• Creator** [Creator Repo](https://t.me/robotrakitangakbagus)\n **• Editor** [Editor Repo](https://t.me/kanjengingsun)",
+        "**╭━━━━━━━━━━━╮**\n        [Repository](https://github.com/AftahBagas/new_userbot)\n**╰━━━━━━━━━━━╯**\n **• Creator** [Creator Repo](https://t.me/robotrakitangakbagus)\n **• Editor** [Editor Repo](https://t.me/kanjengingsun)",
     )
