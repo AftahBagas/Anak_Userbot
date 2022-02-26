ec0939a9853f25c826f5b794f5a928aab3d40dbc
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2022-02-26 13:39:48 +0700

    Update version.py

--- PandaUserbot/version.py
+++ PandaUserbot/version.py
@@ -1,1 +1,19 @@
-__version__ = "💐🐼💐"
+from sys import version_info
+
+branch = f"@PandaUserbot"
+
+__Tgl__ = "25"
+__Bln__ = "02"
+__Thn__ = "2022"
+
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+__license__ = "GNU Affero General Public License v3.0"
+__author__ = "New_UserBot <https://github.com/AftahBagas/New_Userbot>"
+__copyright__ = "PandaX_UserBot Copyright (©) 2020 - 2021  " + __author__
+
+
+versi = f"{__Tgl__}.{__Bln__}.{__Thn__}"
+
+__version__ = versi + "|" + branch
+
